ad1686b28a9ca1ff1528412aaab3e221e4faf903
ongguri <rkdtjsdyd321@naver.com>
2023-02-22 20:22:28 +0900

    ksy

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,8 +1,17 @@
 package practice;
 
-public class Main {
-
+public class Main implements Greeting{
+	
+	public void hi() {
+		String str1 = "안녕하세요.";
+		String str2 = "강선용 입니다.";
+		System.out.println(str1 + str2);
+	}
+	
+	
 	public static void main(String[] args) {
-			
+		
+		Main m = new Main();
+		m.hi();
 	}
 }
